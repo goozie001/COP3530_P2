b7347ec012426caa9ba38bd4ab44f5978c353a0c
DylanRichardson <dylanrichardson47@gmail.com>
2014-11-26 21:22:37 -0500

    Part 1 Fully Implemented but not fully tested

--- OAHM.h
+++ OAHM.h
@@ -1,54 +1,169 @@
 #include <math.h>
 #include <array>
+#include <iostream>
 
 #ifndef OAHM_H
 #define OAHM_H
 
 namespace COP3530 {
-    template <typedef K, typedef V> class OAHM {
+    template <typename K, typename V, typename HASH_FUNC, HASH_FUNC hash, bool double_hash, typename PROBE_FUNC,
+                PROBE_FUNC probe, bool op_equals = true, typename EQ_FUNC = bool, EQ_FUNC eq_test = false>
+    class OAHM {
 
     private:
-
-        const static float A = (sqrt(5) - 1) / 2;
-        unsigned int m;
-        int used;
-        std::array<hashEle>* hashArray;
-
         struct hashEle {
             K key;
             V value;
-            bool isValid;
+            bool invalid;
 
             hashEle() {
-                isValid = false;
+                invalid = true;
+            }
+
+            hashEle(K key, V value) {
+                this->key = key;
+                this->value = value;
+                invalid = false;
+            }
+
+            void set(K key, V value) {
+                this->key = key;
+                this->value = value;
+                invalid = false;
             }
         };
 
-        int hash(K key) {
-            return (int) floor((m * ((A * key) % 1)));
+        std::size_t m;
+        std::size_t used;
+        hashEle* hashArray;
+
+        void initContainers() {
+            for (int i = 0; i < m; ++i)
+                hashArray[i] = *(new hashEle);
         }
 
     public:
 
         OAHM() {
             m = 1024;
-            hashArray = ne
-            for (int i = 0; i < m; ++i) {
-                hashArray[i] = new hashEle();
-            }
-            used = -1;
+            hashArray = new hashEle[m]();
+            used = 0;
+            initContainers();
+        }
+
+        OAHM(std::size_t mySize) {
+            int power = log2(mySize) + 1;
+            m = pow(2, power);
+            const std::size_t a = m;
+            hashArray = new hashEle[m]();
+            used = 0;
+            initContainers();
+        }
+
+        ~OAHM() {
+            delete hashArray;
         }
 
         bool insert(K key, V value) {
             if (used == m)
                 return false;
             else {
+                int i = hash(key, m);
+                if (hashArray[i].invalid) {
+                    hashArray[i].set(key, value);
+                    ++used;
+                    return true;
+                }
+                for (int j = 1; j < m; ++j) {
+                    int p = (i + probe(j)) % m;
+                    if (hashArray[p].invalid) {
+                        hashArray[p].set(key, value);
+                        ++used;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        bool remove(K key, V& value) {
+            if (used == 0)
+                return false;
+            else {
                 int i = hash(key);
-                while (hashArray[i].isValid)
+                if (hashArray[i].key == key && !hashArray[i].invalid) {
+                    value = hashArray[i].value;
+                    hashArray[i].invalid = true;
+                    --used;
+                    return true;
+                } else {
+                    for (int j = 1; j < m; ++j) {
+                        int p = probe(key, j);
+                        if (hashArray[p].key == key && !hashArray[i].invalid) {
+                            value = hashArray[i].value;
+                            hashArray[i].invalid = true;
+                            --used;
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
+        bool search(K key, V& value) {
+            if (used == 0)
+                return false;
+            else {
+                int i = hash(key);
+                if (hashArray[i].key == key && !hashArray[i].invalid) {
+                    value = hashArray[i].value;
+                    return true;
+                } else {
+                    for (int j = 1; j < m; ++j) {
+                        int p = probe(key, j);
+                        if (hashArray[p] != NULL && !hashArray[i].invalid) {
+                            value = hashArray[p].key;
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
+        void clear() {
+            delete hashArray;
+            hashArray = new std::array<hashEle, m>;
+            used = 0;
+        }
+
+        bool is_empty() {
+            return used == 0;
+        }
+
+        std::size_t capacity() {
+            return m;
+        }
+
+        std::size_t size() {
+            return used;
+        }
+
+        double load() {
+            return (double) used / (double) m;
+        }
+
+        void print(std::ostream& out) {
+            for (int i = 0; i < m; ++i) {
+                if (!hashArray[i].invalid) {
+                    out << hashArray[i].key << std::endl;
+                } else {
+                    out << "-" << std::endl;
+                }
             }
         }
     };
-
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
+#include "OAHM.h"
 
+inline int hash(int key, std::size_t m) {
+    const double A = (sqrt(5) - 1) / 2;
+    double intPart;
+    return (int)(m * modf((A / 2.0) * key, &intPart));
+}
+inline int quad_probe(int i) { return (i*(i + 1) / 2); }
 
 int main() {
+    COP3530::OAHM<int, char, int(*)(int,std::size_t), hash,
+            false, int(*)(int), quad_probe> myHash(33);
 
+    myHash.insert(1, 'A');
+    myHash.insert(2, 'B');
+    myHash.insert(3, 'C');
+    myHash.insert(4, 'D');
+    myHash.insert(5, 'E');
+    myHash.insert(6, 'F');
+    myHash.insert(7, 'G');
+    myHash.insert(8, 'H');
+
+    myHash.print(std::cout);
 }
